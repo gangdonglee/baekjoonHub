bf74689e68ff1bdd78c28b11d1fe12f78463c09e
gangdonglee <69913785+gangdonglee@users.noreply.github.com>
2023-04-28 11:28:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int  a = 0;
+	cin >> a;
+	for (int i = 1; i <= a; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
