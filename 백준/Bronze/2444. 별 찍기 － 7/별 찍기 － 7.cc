8528ca3c8f9a56755fc9f551f8669a0c63134eb0
gangdonglee <69913785+gangdonglee@users.noreply.github.com>
2023-04-30 23:48:31 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int  a = 0;
+	cin >> a;
+
+	for (int i = 1; i < a; ++i)
+	{
+		for (int k = i; k < a; ++k)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < i; ++j)
+		{
+			cout << "*";
+		}
+		for (int r = 0; r < i - 1; ++r)
+			cout << "*";
+		cout << "\n";
+	}
+
+
+	for (int i = a; i >= 0; --i)
+	{
+		for (int k = i; k < a; ++k)
+		{
+			cout << " ";
+		}
+		for (int j = i; j > 0; --j)
+		{
+				cout << "*";
+		}
+		for(int r = i -1; r > 0; --r)
+			cout << "*";
+		cout << "\n";
+	}
+}
